4bea5deda47818f065056610882e528b226d3397
swati02-tech <swatid3366@gmail.com>
2021-09-06 20:07:16 +0530

    add changes in feature-101

--- src/main/java/com/example/MvcStudentformApplication.java
+++ src/main/java/com/example/MvcStudentformApplication.java
@@ -8,6 +8,9 @@ public class MvcStudentformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcStudentformApplication.class, args);
-	}
+	
+                 int a=10;
+                 int b=20;
+}
 
 }
